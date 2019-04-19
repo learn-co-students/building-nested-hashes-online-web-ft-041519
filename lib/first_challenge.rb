a097f2330af792c0458adea00af376a4ae417452
Erin Stalzer <erinstalzer@gmail.com>
2019-04-19 01:56:40 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,16 @@
 def first_challenge
-   epic_tragedy = {}
+  epic_tragedy = {
+    :montague => {},
+    :capulet => {}
+  }
+end
+def second_challenge
+  epic_tragedy[:montague] = matriarch
+  epic_tragedy[:montague] = patriarch
+  epic_tragedy[:montague] = heroine
+  epic_tragedy[:montague] = heroine_friends
+  epic_tragedy[:capulet] = matriarch
+  epic_tragedy[:capulet] = patriarch
+  epic_tragedy[:capulet] = heroine
+  epic_tragedy[:capulet] = heroine_friends
 end
